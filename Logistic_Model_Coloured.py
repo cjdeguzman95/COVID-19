86b1d0364e39729de8c068b7787adc21aa220d88
CJ De Guzman <cjdeguzman0@gmail.com>
2020-04-16 13:41:04 +0100

    first commit

---
+++ Logistic_Model_Coloured.py
@@ -0,0 +1,72 @@
+# the following forecasts the state of infections in the future based on specific infection rates
+from vpython import *
+import pandas as pd
+from GroundWork import a_value, retrieve_country_data
+
+
+def curve_graph(graph_number, rate_name, infection_rate, confirmed_cases, population, time):
+
+    a = infection_rate
+    n = confirmed_cases
+    n_max = population
+    time_period = time
+
+    if graph_number == 1:
+        infection_graph = gcurve(color=color.red, label="{} = {}".format(rate_name, a))
+        t = 0
+        dt = 1
+        n0 = 1
+        while t < time_period:
+            n_old = n
+            n = n + a * (1 - n / n_max) * n * dt
+            r = (n - n_old) / dt
+
+            t = t + dt
+
+            infection_graph.plot(t, r)
+    elif graph_number == 2:
+        infection_graph = gcurve(color=color.blue, label="{} = {}".format(rate_name, a))
+        t = 0
+        dt = 1
+        n0 = 1
+        while t < time_period:
+            n_old = n
+            n = n + a * (1 - n / n_max) * n * dt
+            r = (n - n_old) / dt
+
+            t = t + dt
+
+            infection_graph.plot(t, r)
+    else:
+        infection_graph = gcurve(color=color.black, label="{} = {}".format(rate_name, a))
+        t = 0
+        dt = 1
+        n0 = 1
+        while t < time_period:
+            n_old = n
+            n = n + a * (1 - n / n_max) * n * dt
+            r = (n - n_old) / dt
+
+            t = t + dt
+
+            infection_graph.plot(t, r)
+
+
+if __name__ == '__main__':
+    population = 66650000
+    uk_cumulative = retrieve_country_data("COVID19Data.csv", "United Kingdom", "Confirmed")
+    uk_total_infected = uk_cumulative.iloc[-1]
+    uk_df = retrieve_country_data("Newcases_Data.csv", "United Kingdom", "Confirmed")
+    pre_lockdown = uk_df[0:61]
+
+    uk_series = pd.Series(uk_df)
+    pre_lockdown_series = pd.Series(pre_lockdown)
+
+    avg_rate = round(a_value(uk_series, uk_df, population), 3)
+    pre_lockdown_rate = round(a_value(pre_lockdown_series, pre_lockdown, population), 3)
+
+    graph_axes = graph(title= "How long until there are no more new infections in the UK?", xtitle="Time (days)",
+                       ytitle="Number of infections", width=1000, height=650, yscale="linear")
+
+    curve_graph(1, "Current Average", avg_rate, uk_total_infected, population, 1000)  # avg infection rate - red line
+    curve_graph(2, "Pre-Lockdown", pre_lockdown_rate, uk_total_infected, population, 1000)  # pre-lockdown infection rate - blue line
